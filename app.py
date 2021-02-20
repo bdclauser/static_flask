e9899a4cf8d31317bf554fb830458d4f04a59d33
bdclauser <pzljug@hotmail.com>
2021-02-20 17:33:52 -0500

    update login page misc bug fixes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -14,19 +14,18 @@ def home():
 def welcome():
     return render_template('welcome.html')  # renders welcome.html
 
-
-# This starts the server with the 'run()' method
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-# Route for handling the login page
+# Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != error = 'Invalid Credentials. Please try again.'
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
         else:
-            return redirecte(url_for('home')
-                             )
+            return redirect(url_for('home'))
     return render_template('login.html', error=error)
+    
+# This starts the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
+
