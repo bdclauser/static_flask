358901f8cabf445c0e66061b2e44e82cc9a1e11c
bdclauser <pzljug@hotmail.com>
2021-02-21 18:49:07 -0500

    clean-up code

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 class User(db.Model):
 
-    __tablename__="users"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
