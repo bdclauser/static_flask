80cab2c21cbac887fce1705c79d9ee81082fb43d
bdclauser <pzljug@hotmail.com>
2021-02-21 18:48:08 -0500

    update models

--- models.py
+++ models.py
@@ -2,4 +2,18 @@ class User(db.Model):
 
     __tablename__="users"
 
-    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+
+    def __init__(self, email, password, confirmed, paid=False, admin=False, confirmed_on=None):
+        self.email = email
+        self.password = bcrypt.generate_password_has(password)
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
+        self.confirmed = confirmed
+        self.confirmed_on = confirmed_on
